0c5afeea6407926c028b901bed5c7b567ec38686
arpanam <muriki.arpana@gmail.com>
2024-04-07 16:23:14 +0000

    build and deploy files updated

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 #Build docker image
 
-docker build -t myreactapp_img .
+docker build -t capstoneimg .
 
 #Build Docker Container
 
-docker run --name myreactapp_con -d -p 80:80 myreactapp_img
+docker run --name capstonecontainer -d -p 80:80 capstoneimg
