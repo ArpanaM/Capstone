cd1b8d4bef971fc090ce60b2bfa3800f1eb6f824
arpanam <muriki.arpana@gmail.com>
2024-04-08 09:11:55 +0000

    files updated

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 #Build docker image
+docker stop capstonecontainer || true 
+docker rm capstonecontainer || true
 
 docker build -t capstoneimg .
 
 #Build Docker Container
+
+
 
 docker run --name capstonecontainer -d -p 80:80 capstoneimg
