1bde288d8e76ac83cc6da2449304b0a9fbe0b923
JeevaArasu <144244668+JeevaArasu@users.noreply.github.com>
2024-04-08 13:47:13 +0530

    Update deploy1.sh

--- deploy1.sh
+++ deploy1.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
-sudo docker login -u arpanamuriki -p dckr_pat_MiRKO9JlzFxLv7ydo4XDqSzXN3I
-
-if [[ $GIT_BRANCH == 'origin/dev' ]]; then
-
-sudo docker tag myreactapp_img arpanamuriki/dev
-
-sudo docker push arpanamuriki/dev
-
-sudo docker images
-
+docker login -u arpanamuriki -p dckr_pat_MiRKO9JlzFxLv7ydo4XDqSzXN3I
+if [[ $GIT_BRANCH == "origin/master" ]]; then
+docker tag capstone jeevaarasu/prod:cap
+docker push jeevaarasu/prod:cap
+elif [[ $GIT_BRANCH == "origin/dev" ]]; then
+docker tag capstone jeevaarasu/dev:app
+docker push jeevaarasu/dev:app
 fi
